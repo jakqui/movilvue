72181c68c11a3a595c929c5c81f3053b6391a1c4
jakqui <jakqueline24@gmail.com>
2025-03-24 15:28:08 -0600

    certificados y actions

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,11 +1,6 @@
 <template>
   <q-page class="flex flex-center">
     <h5>HOLA MUNDO!!</h5>
-    <img
-      alt="Quasar logo"
-      src="~assets/quasar-logo-vertical.svg"
-      style="width: 200px; height: 200px"
-    >
   </q-page>
 </template>
 
